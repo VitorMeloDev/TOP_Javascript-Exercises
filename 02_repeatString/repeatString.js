c066706ccdeb36b70831237067d591b4d1174209
Vitor Melo <vitormelodev@gmail.com>
2024-05-20 17:00:21 -0300

    Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeats) {
+    if(repeats < 0){return 'ERROR';}
+    let stringVal = '';    
+    for (let i = 0; i < repeats; i++) {
+        stringVal += string;
+    }
+    return stringVal
 };
 
 // Do not edit below this line
