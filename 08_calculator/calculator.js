73b028441c06f13187066208e77997d8452e7f68
Vitor Melo <89869031+VitorMeloDev@users.noreply.github.com>
2024-06-12 16:31:20 -0300

    Exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,52 @@
-const add = function() {
-	
+const add = function(a,b) {
+  if(isNumber(a) && isNumber(b))
+    {
+      return a + b;
+    }
+    else{
+      return;
+    }
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  if(isNumber(a) && isNumber(b))
+    {
+      return a - b;
+    }
+    else{
+      return;
+    }
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, current) => product * current)
 };
 
-const power = function() {
-	
+const power = function(a,b) {  
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
+
+function isNumber(value) {
+  return typeof value === 'number';
+}
+
+function isArray(arr)
+{
+  return Array.isArray(arr);
+}
 
 // Do not edit below this line
 module.exports = {
