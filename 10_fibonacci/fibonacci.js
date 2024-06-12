6c3b028fa87a448bc656d7c410c3145fc2691b98
Vitor Melo <89869031+VitorMeloDev@users.noreply.github.com>
2024-06-12 17:16:53 -0300

    Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(value) {
+    let count = 0;
+    let current = 0;
+    let first = 0;
+    let second = 1;
 
+    if(typeof value !== 'number'){
+        count = parseInt(value);
+    }else{
+        count = value;
+    }
+
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
+
+    for(let i = 1; i < count; i++){
+        current = first + second;
+        first = second;
+        second = current;
+    }
+
+    return second;
 };
 
 // Do not edit below this line
